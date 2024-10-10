c8481bb3f065fd8a82d6497b9a8f76abc2ad7b25
mar-casariego <casariegoom@gmail.com>
2024-10-10 19:55:25 -0300

    validación email

--- js/validacion.js
+++ js/validacion.js
@@ -30,4 +30,19 @@ document.addEventListener("DOMContentLoaded", function() {
     
     terminos.addEventListener("change", validateTerms);
   });
+
+
+// Validación del email
+const email = document.getElementById('email');
+
+email.addEventListener('input', function () {
+  this.value = this.value.trim(); // Elimina espacios
+  if (this.checkValidity()) {
+    this.classList.remove('is-invalid');
+    this.classList.add('is-valid');
+  } else {
+    this.classList.remove('is-valid');
+    this.classList.add('is-invalid');
+  }
+});
   
